a9e4b45bd27e224a986a8bd23f96f1c5cc3e59a2
Tanvi Yamarthy <125161619+tyamarthy@users.noreply.github.com>
2025-02-11 18:59:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import CareerPaths from './components/CareerPaths';
 import CareerQuiz from './components/CareerQuiz';
 import SkillRoadmap from './components/SkillRoadmap';
 import Mentorship from './components/Mentorship';
-import Dashboard from './components/Dashboard'; // Import Dashboard for the signed-in page
-import Navbar from './components/Navbar';  // New Navbar component
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar'; 
 
 function App() {
-  const [user, setUser] = useState(null); // Track if the user is signed in
+  const [user, setUser] = useState(null); 
 
   return (
     <Router>
       <Routes>
-        {/* Conditional Rendering: Only show Navbar if user is signed in */}
+        {/* Conditional Rendering:  show Navbar if user is signed in */}
         <Route path="/" element={<HomePage setUser={setUser} />} />
         
-        {user && <Navbar />} {/* Show Navbar only if user is signed in */}
+        {user && <Navbar />} {/* nav only if user is signed in */}
 
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/career-paths" element={<CareerPaths />} />
